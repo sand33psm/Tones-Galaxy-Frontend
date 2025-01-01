1473f29756cb8dd00ae1f86c9c9a574c18cdf22d
sndp <sand33psm@gmail.com>
2025-01-02 00:09:11 +0530

    add sitemap url for individual ringtones dynamically

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,11 +1,24 @@
 import {MetadataRoute} from 'next'
 import { NEXT_PUBLIC_BASE_URL } from '@/constants'
+import { fetchRingtones } from '@/services/ringtonesService'
 
 export default async function sitemap() {
+    const ringtones = await fetchRingtones()
+
+    const ringtoneEntries = ringtones.map(({id}) => ({
+        url: `${NEXT_PUBLIC_BASE_URL}/ringtones/${id}`,
+    }))
+
     
     return [
         {
+            url: `${NEXT_PUBLIC_BASE_URL}`
+        },
+        {
             url: `${NEXT_PUBLIC_BASE_URL}/ringtones`
-        }
+        },
+        ...ringtoneEntries
+        
+
     ]
 }
